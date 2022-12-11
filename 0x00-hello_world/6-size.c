7d9b7ef4fff48935f6e816ad8a6e1117a07a6029
masha-a-m <escritoramaree@gmail.com>
2022-12-11 02:32:57 -0800

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
-
 /**
- *main - print 0123456789 using putchar
- *Return: Always 0 (Success)
+ *main - Entry
+ *Return: Always 0
  */
-
 int main(void)
 {
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
 
-     int n = '0';
-
-     while (n <= '9')
-     {					
-
-	 putchar(n);
- 	 n++;	
-
-     }    
-     
-     putchar('\n');
-
-     return (0);
-
-}
