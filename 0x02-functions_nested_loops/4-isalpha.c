1a09ab8167e5348ed9867a3bd21eeb956bb7e8cb
masha-a-m <escritoramaree@gmail.com>
2022-12-14 12:49:10 -0800

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,22 +1,14 @@
-#include "holberton.h"
+#include "main.h"
 /**
- * _isalpha - Check Main
- * @c: An input character
- * Description: function returns 1 if the character is a
- * letter, lowercase or uppercase.
- * Return: 1 or 0 in otherwise.
+ * _isalpha - checks for alphabetic character
+ * @c: the parameter of the function
+ *
+ * Return: 1 (Success)
  */
 int _isalpha(int c)
 {
-char lower, upper;
-int isletter = 0;
-for (lower = 'a'; lower <= 'z'; lower++)
-{
-for (upper = 'A'; upper <= 'Z'; upper++)
-{
-if (c == lower || c == upper)
-isletter = 1;
-}
-}
-return (isletter);
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return (1);
+else
+return (0);
 }
