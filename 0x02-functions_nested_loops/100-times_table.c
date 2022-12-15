03a32d32ad5d81948f550f273e69d02a49970e86
masha-a-m <escritoramaree@gmail.com>
2022-12-15 12:27:59 -0800

    Task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,13 +15,14 @@ _putchar(',');
 _putchar(' ');
 prod = i * j;
 if (prod <= 99)
-_putchar(' ');	
+_putchar(' ');
 if (prod <= 9)
 _putchar(' ');
 if (prod >= 100)
 {
 _putchar((prod / 100) + '0');
-_putchar(((prod / 10)) % 10 + '0');											}
+_putchar(((prod / 10)) % 10 + '0');
+}
 else if (prod <= 99 && prod >= 10)
 {
 _putchar((prod / 10) + '0');
