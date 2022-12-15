647bf6bb649c0a02c3e2d2611f5c896552b7396e
masha-a-m <escritoramaree@gmail.com>
2022-12-15 12:22:37 -0800

    Task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,8 +25,9 @@ _putchar(((prod / 10)) % 10 + '0');											}
 else if (prod <= 99 && prod >= 10)
 {
 _putchar((prod / 10) + '0');
-}															_putchar((prod % 10) + '0');												}
-_putchar('\n');
+}
+_putchar((prod % 10) + '0');
+}															_putchar('\n');
 }
 }
 }
