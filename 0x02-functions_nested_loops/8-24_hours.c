f39d5f726da022c0714b950327431eba7fc670ec
masha-a-m <escritoramaree@gmail.com>
2022-12-15 10:58:08 -0800

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,7 +15,8 @@ _putchar((hour / 10) + '0');
 _putchar((hour % 10) + '0');
 _putchar(':');
 _putchar((min / 10) + '0');
-_putchar((min % 10) + '0');												_putchar('\n');
+_putchar((min % 10) + '0');
+_putchar('\n');
 }
 }
 }
