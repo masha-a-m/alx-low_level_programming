26d9caa82abd3dd50be29ff0001226d006c05b03
masha-a-m <escritoramaree@gmail.com>
2022-12-30 02:49:32 -0800

    Task 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,35 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- *main - print a num pair from 00-99 but no repeats (00 01, 00 02, 00 03,...)
- *Return: Always 0 (Success)
+ * main - return program prints all possible combinations of 2 2-digit numbers.
+ * Return: return 0 (Success).
  */
 int main(void)
 {
-int tens;
-int ones;
-int t;
-int o;
-for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
+int i, j;
+for (i = 0; i < 100; ++i)
 {
-for (ones = '0'; ones <= '9'; ones++)
+for (j = 0; j < 100; ++j)
 {
-for (t = tens; t <= '9'; t++) /*print second of pair*/
+if (i < j)
 {
-for (o = ones + 1; o <= '9'; o++)
-{
-putchar(tens);
-putchar(ones);
-putchar(' ');
-putchar(t);
-putchar(o);
-if (!((tens == '9' && ones == '8') &&
-(t == '9' && o == '9')))
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
+putchar((' '));
+putchar((j / 10) + 48);
+putchar((j % 10) + 48);
+if (i != 98 || j != 99)
 {
 putchar(',');
-putchar(' ')
+putchar(' ');
 }
-}
-o = '0';
 }
 }
 }
