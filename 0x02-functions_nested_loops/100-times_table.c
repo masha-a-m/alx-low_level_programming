b810e6bbdf3f82413a086cac2dfb35819d7f103b
masha-a-m <escritoramaree@gmail.com>
2022-12-30 03:12:30 -0800

    Tash 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * print_times_table - prints times table
- * @n : times table to use
+ * print_times_tables - prints times tables
+ * @n : times tables to use
  * Return: void
  */
-void print_times_table(int n)
+void print_times_tables(int n)
 {
 int a = 0, rep, b;
 if (n < 0 || n > 15)
