ffa5224685995d8efda1f87dd670568254abce53
masha-a-m <escritoramaree@gmail.com>
2023-01-02 11:09:39 -0800

    3 Print

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -27,7 +27,8 @@ else
 if (month == 2 && day == 60)
 {
 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-}										else
+}
+else
 {
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
