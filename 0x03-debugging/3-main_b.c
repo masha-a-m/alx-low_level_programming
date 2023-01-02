756237089d360aba5680732114e162eae4ef8953
masha-a-m <escritoramaree@gmail.com>
2023-01-02 10:58:08 -0800

    Task 3 B

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -20,6 +20,7 @@ year = 2000;
 printf("Date: %02d/%02d/%04d\n", month, day, year);
 
 day = convert_day(month, day);
-										print_remaining_days(month, day, year);						
+
+print_remaining_days(month, day, year);						
 										return (0);
 }
