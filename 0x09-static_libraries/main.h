ee90f999a7c7845fdbe0e4731eecea0d9e5994cd
masha-a-m <escritoramaree@gmail.com>
2023-01-16 12:36:38 -0800

    Main

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,10 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * Header file containing function prototypes
- * used in the static library
- */
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -25,5 +20,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
 #endif
