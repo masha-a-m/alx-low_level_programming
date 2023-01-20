79c22e03e1cef8ba294e823a9a7bc334311b19ad
masha-a-m <escritoramaree@gmail.com>
2023-01-20 07:16:51 -0800

    Task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-/** 		
- * _strdup - duplicate to new memory space location 		 		
+/**
+ * _strdup - duplicate to new memory space location
  * @str: char
  * Return: 0
  */
