c94b6cfea5b67f3153beaccc0bd75f710c728266
masha-a-m <escritoramaree@gmail.com>
2023-01-20 07:19:13 -0800

    Task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,7 +16,8 @@ i = 0;
 while (str[i] != '\0')
 i++;
 aaa = malloc(sizeof(char) * (i + 1));
-if (aaa == NULL)return (NULL);
+if (aaa == NULL)
+return (NULL);
 for (r = 0; str[r]; r++)
 aaa[r] = str[r];
 return (aaa);
