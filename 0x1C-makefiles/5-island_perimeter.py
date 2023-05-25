a5df521b64a6a9b7c667427622596b4be756faac
masha-a-m <escritoramaree@gmail.com>
2023-05-24 17:22:31 -0700

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,10 +7,11 @@ def island_perimeter(grid):
 
 #Create a function def island_perimeter(grid): that returns the perimeter of the island described in grid:
 
-width = len(grid[0]) """fot the width"""
+    width = len(grid[]) 
+	 """fot the width"""
     
-height = len(grid) """height"""
-
+    height = len(grid) 
+	 """height"""
     edges = 0
     size = 0
 
@@ -22,21 +23,23 @@ height = len(grid) """height"""
 #One cell is a square with side length 1
 #Grid cells are connected horizontally/vertically (not diagonally).
 
-for i in range(height): """for every i in the range height"""
+     for i in range(height): 
+"""for every i in the range height"""
 
-        for j in range(width):
-            if grid[i][j] == 1: """for jin ramge width"""
+         for j in range(width):
+             if grid[i][j] == 1: 
+"""for jin ramge width"""
 
-                size += 1
-                if (j > 0 and grid[i][j - 1] == 1):
-                    edges += 1
+                 size += 1
+                 if (j > 0 and grid[i][j - 1] == 1):
+                     edges += 1
             
 
-		  if (i > 0 and grid[i - 1][j] == 1):
-                    edges += 1
+		   if (i > 0 and grid[i - 1][j] == 1):
+                     edges += 1
 
 
-	return size * 4 - edges * 2
+     return size * 4 - edges * 2
 #Grid is rectangular, width and height don’t exceed 100
 #Grid is completely surrounded by water, and there is one island (or nothing).
 #The island doesn’t have “lakes” (water inside that isn’t connected to the water around the island).
