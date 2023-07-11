46c3099f843065b024082c2092f23383974fc369
masha-a-m <escritoramaree@gmail.com>
2023-07-11 12:34:33 -0700

    u

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,7 +2,12 @@
 
 /**
   task 0
-  */
+ n that searches for a value in an array of integers using the Linear search algorithm
+
+Prototype : int linear_search(int *array, size_t size, int value);
+Where array is a pointer to the first element of the array to search in
+size is the number of elements in array 
+ */
 
 for (i = 0; i < (int)size; i++)
 	{
